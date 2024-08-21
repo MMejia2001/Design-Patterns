d4cc0845eeedad098e47c1857e266106ea6c4139
MMejia2001 <Marco.Mejia@axity.com>
2024-08-21 00:47:31 +0000

    Creacion de StudentUIFactory - InstructorUIFactory

--- Creacionales/Abstract-Factory/index.ts
+++ Creacionales/Abstract-Factory/index.ts
@@ -1,10 +1,25 @@
 
 import { AdminUIFactory } from './AdminUIFactory';
+import { InstructorUIFactory } from './InstructorUIFactory';
+import { StudentUIFactory } from './StudentUIFactory';
 import { UIManager } from './UIManager';
 
 const uifactory = new AdminUIFactory();
+const uifactory2 = new StudentUIFactory();
+const uifactory3 = new InstructorUIFactory();
+
 const uiManager = new UIManager(uifactory);
+const uiManager2 = new UIManager(uifactory2);
+const uiManager3 = new UIManager(uifactory3);
 
 const adminUI = uiManager.createUI();
 adminUI.button.render();
 adminUI.dialog.show();
+
+const studentUI = uiManager2.createUI();
+studentUI.button.render();
+studentUI.dialog.show();
+
+const instructorUI = uiManager3.createUI();
+instructorUI.button.render();
+instructorUI.dialog.show();
